49ce2e0365a619c27e6f60e03bf61bce6e14caa7
Stellaz1b <yibei@umich.edu>
2025-11-08 11:59:22 -0500

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by zzz on 11/8/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
